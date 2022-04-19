7664bc4f4f158d64517ae9f40e1701fc0875fcfe
khanhdrag9 <tkhanh783@gmail.com>
2022-04-20 00:52:47 +0700

    updated base

---
+++ Assets/Scripts/Wager/SingleNumberWager.cs
@@ -0,0 +1,20 @@
+namespace Game
+{
+    public class SingleNumberWager : WagerBase
+    {
+        public override WagerType WagerType => WagerType.Single;
+
+        public int BetNumber {get;}
+
+        public SingleNumberWager(int betAmount, float bonusRate, int betNumber) : base(betAmount, bonusRate)
+        {
+            this.BetNumber = betNumber;
+        }
+
+
+        public override bool IsRewardAble()
+        {
+            return PlayBoardManager.Instance.Result == BetNumber;
+        }
+    }
+}
